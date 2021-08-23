e7e7e0076a824cd82ffb666ad5f55595266bada9
Shariq Hirani <9244782+shariqh@users.noreply.github.com>
2021-08-23 11:54:40 -0500

    update callout to span

--- components/Callout.js
+++ components/Callout.js
@@ -17,8 +17,10 @@ export default function Callout({ text, linkText, link }) {
           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
-      &nbsp;{text}&nbsp;
-      <Link href={link}>{linkText}</Link>
+      <span>
+        &nbsp;{text}&nbsp;
+        <Link href={link}>{linkText}</Link>
+      </span>
     </div>
   )
 }
