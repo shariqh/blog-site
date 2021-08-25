565a542bf8787699a5be008a0b2513f521c1050c
Shariq Hirani <9244782+shariqh@users.noreply.github.com>
2021-08-25 16:13:21 -0500

    fix subscribe button for xl

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -43,7 +43,7 @@ export default function Subscribe() {
   }
 
   return (
-    <div className="rounded-xl p-6 my-4 w-full bg-gray-50 dark:bg-gray-700">
+    <div className="rounded-xl p-6 xl:p-4 my-4 xl:my-4 w-full bg-gray-50 dark:bg-gray-700">
       <p className="text-lg md:text-xl font-bold text-gray-900 dark:text-gray-100">
         Want more from me?
       </p>
@@ -51,7 +51,7 @@ export default function Subscribe() {
         Subscribe to my newsletter and get emails about leadership, tech, and early access to new
         articles.
       </p>
-      <form className="relative my-4" onSubmit={subscribe}>
+      <form className="relative my-4 xl:my-2" onSubmit={subscribe}>
         <input
           ref={inputEl}
           aria-label="Email for newsletter"
@@ -62,7 +62,7 @@ export default function Subscribe() {
           className="px-4 py-2 mt-1 focus:ring-primary-500 focus:border-primary-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
         />
         <button
-          className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 focus:bg-primary-200 text-gray-900 dark:text-gray-100 rounded w-28"
+          className="flex items-center justify-center absolute xl:relative right-1 xl:right-0 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 focus:bg-primary-200 text-gray-900 dark:text-gray-100 xl:border rounded w-28"
           type="submit"
         >
           {form.state === 'loading' ? <LoadingSpinner /> : 'Subscribe'}
